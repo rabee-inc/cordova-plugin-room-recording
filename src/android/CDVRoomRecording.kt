d553e230ab4ac2ef6cd0a5b141455b2ee0c9abd5
nekokuloid <nekokuloid@gmail.com>
2020-06-22 13:29:42 +0900

    joinRoom, leaveRoomのAPIを作成

--- src/android/CDVRoomRecording.kt
+++ src/android/CDVRoomRecording.kt
@@ -1,10 +1,15 @@
 package jp.rabee
 
-import org.apache.cordova.*
-import org.json.JSONException
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
 import android.view.WindowManager
-import org.json.*
+import io.agora.rtc.IRtcEngineEventHandler
+import io.agora.rtc.RtcEngine
+import org.apache.cordova.*
+import org.json.JSONArray
 
 
 class CDVRoomRecording : CordovaPlugin() {
@@ -13,11 +18,18 @@ class CDVRoomRecording : CordovaPlugin() {
     // 別の callback context を用意する
     lateinit var onProgressCallbackContext: CallbackContext
 
+    // AgoraEngine
+    private var mRtcEngine: RtcEngine? = null
+    private val PERMISSION_REQ_ID_RECORD_AUDIO = 22
+
     // アプリ起動時に呼ばれる
     override public fun initialize(cordova: CordovaInterface,  webView: CordovaWebView) {
         LOG.d(TAG, "hi! This is CDVKeepAwake. Now intitilaizing ...");
-    }
 
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO)) {
+            initializeAgoraEngine();
+        }
+    }
 
     // js 側で関数が実行されるとこの関数がまず発火する
     override fun execute(action: String, data: JSONArray, callbackContext: CallbackContext): Boolean {
@@ -30,11 +42,16 @@ class CDVRoomRecording : CordovaPlugin() {
             "stop" -> {
                 result = this.stop(context)
             }
+            "joinRoom" -> {
+                result = this.joinChannel()
+            }
+            "leaveRoom" -> {
+                result = this.leaveChannel()
+            }
             else -> {
                 // TODO error
             }
         }
-
 
         return result
     }
@@ -67,5 +84,63 @@ class CDVRoomRecording : CordovaPlugin() {
 
     companion object {
         protected val TAG = "CDVKeepAwake"
+    }
+
+    // Call the create method to initialize RtcEngine.
+    private fun initializeAgoraEngine() {
+        // initialize AgoraEngine
+        try {
+            // TODO: context取得方法これで正しいか不明
+            var activity: Activity = this.cordova.getActivity()
+            var baseContext: Context = activity.getApplicationContext();
+            mRtcEngine = RtcEngine.create(baseContext, activity.getString(R.string.agora_app_id), mRtcEventHandler)
+        } catch (e: Exception) {
+            Log.e(TAG, Log.getStackTraceString(e))
+            throw RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e))
+        }
+    }
+
+    private val mRtcEventHandler = object : IRtcEngineEventHandler() {
+        // Listen for the onUserOffline callback.
+        // This callback occurs when the remote user leaves the channel or drops offline.
+        override fun onUserOffline(uid: Int, reason: Int) {
+            // TODO: ???
+            // runOnUiThread { onRemoteUserLeft() }
+        }
+
+        // Listen for the onUserMuterAudio callback.
+        // This callback occurs when a remote user stops sending the audio stream.
+        override fun onUserMuteAudio(uid: Int, muted: Boolean) {
+            // TODO: ???
+            // runOnUiThread { onRemoteUserVoiceMuted(uid, muted)}
+        }
+    }
+
+    /**
+     * パーミッション保有チェック
+     */
+    private fun checkSelfPermission(permission: String, requestCode: Int): Boolean {
+        Log.i(TAG, "checkSelfPermission $permission $requestCode")
+        if (ContextCompat.checkSelfPermission(this,
+                        permission) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermission(this
+                    arrayOf(permission),
+                    requestCode)
+            return false
+        }
+        return true
+    }
+
+    private fun joinChannel(): Boolean {
+        // TODO: トークン取得
+        val token: String = ""
+        mRtcEngine!!.joinChannel(token, "demoChannel1", "Extra Optional Data", 0)
+        return true
+    }
+
+    private fun leaveChannel(): Boolean{
+        mRtcEngine!!.leaveChannel()
+        return true
     }
 }
